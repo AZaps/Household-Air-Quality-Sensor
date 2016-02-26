07d7c7101ba12cc34404e4adc3db87894c6df4b9
Anthony Zaprzalka <azmail@comcast.net>
2016-02-26 13:42:36 -0600

    Updates to saving SD card. Crashes if SD card is removed(temporary fix in place). Still not saving correctly. Somehow skipping over a section to get the date when it is time to save the sensor data.

--- HomeAirQualitySensor/HomeAirQualitySensor.ino
+++ HomeAirQualitySensor/HomeAirQualitySensor.ino
@@ -44,15 +44,15 @@ char directoryPath[21] = "/sendata/";
 char fullInput[32];                   // The full input of Date/Time/Data
 char directoryName[8];
 char currentSavingDateTime[16];
-char intBuffer[16];
+char convertBuffer[16];
 
 // Sensor variables
 float sensorVoltage;
 float sensorValue;
 int sensorCounter = 0;                // Counter variable for selecting sensor
-int totalAmountOfSensors = 4;         // CHANGE DEPENDING ON AMOUNT OF SENSORS
+long totalAmountOfSensors = 4;        // CHANGE DEPENDING ON AMOUNT OF SENSORS
 long averageRunCounter = 0;
-int sensorAverageArray[4];            // Averages of all the sensors through the minute
+long sensorAverageArray[4];           // Averages of all the sensors through the minute
 
 // Interrupt variables
 long oneMinuteDelayCounter = 0;       // 60,000 milliseconds in a minute, resets once data has been saved
@@ -96,6 +96,7 @@ SIGNAL(TIMER0_COMPA_vect) {
   oneMinuteDelayCounter++;
   if (oneMinuteDelayCounter == 60000) {
     oneMinute = true;
+    Serial.println("One minute has passed.");
   }
 } // END OF INTERRUPT
 
@@ -103,70 +104,29 @@ void loop() {
   sensorCounter = getSensorData(sensorCounter);
   
   // Only re-initializes the SD card if it was removed or not inserted from the start
-  if (!isSDInserted) {
+  if (isSDInserted == true) {
+    // Need to talk real quick to see if there is an SD card inserted
+  } else if (isSDInserted == false) {
     // Go to function to (re)enable the SD card
     clarity = sdCardFunctions.initializeSD(53,49);
     sdInitCheck(clarity);
   }
 
-    // Bool value changed from interrupts to see if a minute has passed and need to save data to sensor file
-  if (oneMinute) {
-    // Can't save any data if there isn't a SD card...
-    if (!isSDInserted) {
-      Serial.println("SD card currently not found...");
-      Serial.print("Checking for SD card...");
-      clarity = sdCardFunctions.initializeSD(53,49);
-      sdInitCheck(clarity);
-      if (!clarity) {
-         Serial.println("SD card not found, NOT saving data.");
-         clearInterruptVariables();
-      } else {
-        Serial.println("SD card was found, saving sensor data");
-        }
-    } else {
-    // For debugging
-    showFreeMemory();
-      
-      // Get the current dateTime so each sensor saves at the same time
-      strcat(currentSavingDateTime, getDateTimeData());
-      Serial.print("Current dateTime to be saved: ");
-      Serial.println(currentSavingDateTime);
-      // Loop through to save each sensor to the SD card
-      for (int i = 0; i == totalAmountOfSensors; i++) {
-        // Concatenate the date to the full input
-        strcat(fullInput, currentSavingDateTime);
-        // Get the average value
-        sensorAverageArray[i] = sensorAverageArray[i]/averageRunCounter;
-        // Convert to an ASCII character
-        itoa(sensorAverageArray[i], intBuffer, 10);
-        // Concatenate the data to the full input
-        strcat(fullInput, intBuffer);               // Full input set for current sensor
-        memset(intBuffer, 0, sizeof(intBuffer));
-        // Convert sensor postion to ASCII and concatenate onto directory filename i.e. "/sendata/sen(insert sensor number).txt"
-        itoa(i, intBuffer, 10);
-        strcat(directoryPath, "sen");
-        strcat(directoryPath, intBuffer);
-        strcat(directoryPath, ".txt");
-        // Save current sensor data to the SD card
-        Serial.print("For sensor ");
-        Serial.print(i);
-        Serial.print(" we are saving: ");
-        Serial.print(fullInput);
-        Serial.print(". To: ");
-        Serial.println(directoryPath);
-        saveSensorReadings(directoryPath, fullInput);
-        // Reset pathname and data input
-        clearCharPathnames();
-        // Set the current sensor data to 0
-        sensorAverageArray[i] = 0;
-      }
-    }
+  // Need to talk real quick to see if there is an SD card inserted
 
-  // For debugging
-  showFreeMemory();
-  
-  clearInterruptVariables();
+  // Check bool oneMinute value to see if true
+  if (oneMinute) {
+    // Check to see if SD is inserted
+    if (isSDInserted) {
+      Serial.println("The SD card is inserted. Proceeding to save data.");
+      // Go to function to save the data
+      oneMinuteSave();
+    } else {
+      Serial.println("The SD card is not inserted. Not saving data. Will check again on next pass.");
+    }
+    clearInterruptVariables();
   }
+  
 
 } // END OF LOOP
 
@@ -278,31 +238,13 @@ void createFilesystem() {
 
 /* Switches through the sensors to get the data */
 int getSensorData(int sensorNumber) {
-  /* Might not need this commented section since it is not saving anymore in this section, will leave in temporarly
-  switch (sensorNumber) {
-    case 0:
-      strcat(directoryPath, "sen0.txt");
-      break;
-    case 1:
-      strcat(directoryPath, "sen1.txt");
-      break;
-    case 2:
-      strcat(directoryPath, "sen2.txt");
-      break;
-    case 3:
-      strcat(directoryPath, "sen3.txt");
-      break;
-    case 4:
-      strcat(directoryPath, "sen4.txt");
-      break;
-  } */
-
   // Get date and time when either the data needs to be saved after a minute or if the user wants to see the currently running info
 
   sensorAverageArray[sensorNumber] = sensorAverageArray[sensorNumber] + readSensor(sensorNumber);
-
-  // Clear out the variables for next time
-  clearCharPathnames();
+  Serial.print("On sensor number: ");
+  Serial.print(sensorNumber);
+  Serial.print(". Has a value of: ");
+  Serial.println(sensorAverageArray[sensorNumber]);
 
   // Increment sensor number and return
   sensorNumber++;
@@ -354,6 +296,9 @@ char* getDateTimeData() {
   strcat(fullDateTime, tempDateTime);
   strcat(fullDateTime, " ");                            // xx/xx/xx xx:xx(space)
 
+  Serial.print("The returning dateTime is: ");
+  Serial.println(fullDateTime);
+
   return fullDateTime;
 }
 
@@ -367,6 +312,43 @@ float readSensor(int currentSensor) {
   return sensorValue;
 }
 
+/* After a minute this function will be called to save the value of each sensor */
+void oneMinuteSave() {
+  // Get the date first so the data will all be saved at the same time
+  strcat(currentSavingDateTime, getDateTimeData());
+  Serial.print("The current dateTime to be saved to all sensors: ");
+  Serial.println(currentSavingDateTime);
+
+  // Save all sensor data
+  for (int i = 0; i <= totalAmountOfSensors; i++) {
+    // Concatenate the date to the fullInput
+    strcat(fullInput, currentSavingDateTime);
+
+    // Get average value for sensor
+    sensorAverageArray[i] = sensorAverageArray[i] / averageRunCounter;
+    // Convert to an ascii character
+    itoa(sensorAverageArray[i], convertBuffer, 10);
+    // Append to the fullInput
+    strcat(fullInput, convertBuffer);
+
+    // Convert current sensor number to ascii
+    int tempPos = i;
+    itoa(tempPos, convertBuffer, 10);
+    // Add to directoryPath
+    strcat(directoryPath, "sen");
+    strcat(directoryPath, "0");
+    strcat(directoryPath,".txt");
+
+    // Send to be saved
+    Serial.print("INPUT TO BE SAVED: ");
+    Serial.print(fullInput);
+    Serial.print(". AT DIRECTORYPATH: ");
+    Serial.println(directoryPath);
+
+    saveSensorReadings(directoryPath, fullInput);
+    clearCharPathnames();
+  }
+}
 
 /* Saves the full input to the SD card */
 void saveSensorReadings(char* saveTo, char* dataToSave) {
@@ -395,6 +377,13 @@ void clearInterruptVariables() {
   oneMinuteDelayCounter = 0;
   // Reset the oneMinute bool
   oneMinute = false;
+  for (int i = 0; i <= totalAmountOfSensors; i++) {
+    sensorAverageArray[i] = 0;
+    Serial.print("Sensor number: ");
+    Serial.print(i);
+    Serial.print(". Value is: ");
+    Serial.println(sensorAverageArray[i]);
+  }
 }
 
 
